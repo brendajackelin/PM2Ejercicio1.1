328ef5971f052f25f7ad632ed636fd5d1825f916
Petita <Petita@DESKTOP-NI1J1PI>
2022-06-22 00:41:43 -0600

    Agregar archivos de proyecto.

---
+++ Ejercici11/MainPage.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Ejercici11
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnSuma_Clicked(object sender, EventArgs e)
+        {
+            double resultado;
+            double valor1, valor2;
+
+            valor1 = double.Parse(num1.Text);
+            valor2 = double.Parse(num2.Text);
+
+            resultado = valor1 + valor2;
+            await Navigation.PushAsync(new Page2(resultado));
+        }
+
+        private async void btnResta_Clicked(object sender, EventArgs e)
+        {
+            double resultado;
+            double valor1, valor2;
+
+            valor1 = double.Parse(num1.Text);
+            valor2 = double.Parse(num2.Text);
+
+            resultado = valor1 - valor2;
+            await Navigation.PushAsync(new Page2(resultado));
+        }
+
+        private async void btnMultiplicacion_Clicked(object sender, EventArgs e)
+        {
+            double resultado;
+            double valor1, valor2;
+
+            valor1 = double.Parse(num1.Text);
+            valor2 = double.Parse(num2.Text);
+
+            resultado = valor1 * valor2;
+            await Navigation.PushAsync(new Page2(resultado));
+        }
+
+        private async void btnDivision_Clicked(object sender, EventArgs e)
+        {
+            double resultado;
+            double valor1, valor2;
+
+            valor1 = double.Parse(num1.Text);
+            valor2 = double.Parse(num2.Text);
+
+            resultado = valor1 / valor2;
+            await Navigation.PushAsync(new Page2(resultado));
+        }
+    }
+}
---
+++ Ejercici11/Page2.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Ejercici11
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page2 : ContentPage
+    {
+        public Page2(double resultado)
+        {
+            InitializeComponent();
+            res.Text = Convert.ToString(resultado);
+        }
+    }
+}
